c42259369edd2b9c84068ffa642fe334ca1137d3
Степан Климов <stepan.chik228@mail.ru>
2025-10-26 18:23:21 +0500

    Создание классов. Климов

---
+++ Regex_Klimov/Classes/Common/CheckRegex.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Regex_Klimov.Classes.Common
+{
+    public class CheckRegex
+    {
+        public static bool Match(string Pattern, string Input)
+        {
+            Match m = Regex.Match(Pattern, Input);
+            return m.Success;
+        }
+    }
+}
