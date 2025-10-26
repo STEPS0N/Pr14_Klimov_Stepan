91938f3ff0769d96330ec6d0b22e14b7251288e1
Степан Климов <stepan.chik228@mail.ru>
2025-10-26 23:41:26 +0500

    Метод поиска по ФИО. Климов

--- Regex_Klimov/MainWindow.xaml.cs
+++ Regex_Klimov/MainWindow.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Collections.Generic;
 
 namespace Regex_Klimov
 {
@@ -25,6 +26,7 @@ namespace Regex_Klimov
         {
             InitializeComponent();
             init = this;
+            LoadSampleData();
         }
 
         public void LoadPassport()
@@ -66,5 +68,101 @@ namespace Regex_Klimov
             }
         }
 
+        private void ChangeText(object sender, TextChangedEventArgs e)
+        {
+            string search = Search.Text.Trim().ToLower();
+            List<Passport> filteredPassport;
+
+            if (string.IsNullOrEmpty(search))
+            {
+                filteredPassport = Passports;
+            }
+            else
+            {
+                filteredPassport = Passports.Where(x =>x.Name.ToLower().Contains(search) || x.Firstname.ToLower().Contains(search) || x.Lastname.ToLower().Contains(search)).ToList();
+            }
+
+            lv_passport.Items.Clear();
+            
+            foreach (Passport passport in filteredPassport)
+            {
+                lv_passport.Items.Add(passport);
+            }
+        }
+
+        private void LoadSampleData()
+        {
+            Passports.Add(new Passport
+            {
+                Name = "Иван",
+                Firstname = "Иванов",
+                Lastname = "Иванович",
+                Issued = "ОУФМС России по г. Москве",
+                DateOfIssued = "15.05.2020",
+                DepartmentCode = "770-101",
+                SeriesAndNumber = "4510 123456",
+                DateOfBirth = "12.03.1985",
+                PlaceOfBirth = "г. Москва",
+                Picture = "pack://application:,,,/Images/passport.jpg"
+            });
+
+            Passports.Add(new Passport
+            {
+                Name = "Мария",
+                Firstname = "Петрова",
+                Lastname = "Сергеевна",
+                Issued = "ОУФМС России по г. Санкт-Петербургу",
+                DateOfIssued = "20.08.2019",
+                DepartmentCode = "780-102",
+                SeriesAndNumber = "4611 654321",
+                DateOfBirth = "25.07.1990",
+                PlaceOfBirth = "г. Санкт-Петербург",
+                Picture = "pack://application:,,,/Images/passport.jpg"
+            });
+
+            Passports.Add(new Passport
+            {
+                Name = "Алексей",
+                Firstname = "Сидоров",
+                Lastname = "Владимирович",
+                Issued = "ОУФМС России по г. Екатеринбургу",
+                DateOfIssued = "10.12.2021",
+                DepartmentCode = "660-103",
+                SeriesAndNumber = "5012 789012",
+                DateOfBirth = "03.11.1988",
+                PlaceOfBirth = "г. Екатеринбург",
+                Picture = "pack://application:,,,/Images/passport.jpg"
+            });
+
+            Passports.Add(new Passport
+            {
+                Name = "Елена",
+                Firstname = "Козлова",
+                Lastname = "Андреевна",
+                Issued = "ОУФМС России по г. Новосибирску",
+                DateOfIssued = "05.03.2018",
+                DepartmentCode = "540-104",
+                SeriesAndNumber = "5213 345678",
+                DateOfBirth = "18.09.1992",
+                PlaceOfBirth = "г. Новосибирск",
+                Picture = "pack://application:,,,/Images/passport.jpg"
+            });
+
+            Passports.Add(new Passport
+            {
+                Name = "Дмитрий",
+                Firstname = "Смирнов",
+                Lastname = "Олегович",
+                Issued = "ОУФМС России по г. Казани",
+                DateOfIssued = "22.11.2022",
+                DepartmentCode = "160-105",
+                SeriesAndNumber = "5314 987654",
+                DateOfBirth = "30.01.1987",
+                PlaceOfBirth = "г. Казань",
+                Picture = "pack://application:,,,/Images/passport.jpg"
+            });
+
+            LoadPassport();
+        }
     }
 }
