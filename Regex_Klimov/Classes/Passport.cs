783e7f51930e4bdbffefe68c672e0e8480be15eb
Степан Климов <stepan.chik228@mail.ru>
2025-10-26 18:17:42 +0500

    Создание окна добавления/изменения. Климов

---
+++ Regex_Klimov/Classes/Passport.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Regex_Klimov.Classes
+{
+    public class Passport
+    {
+        public string Name { get; set; }
+
+        public string Firstname { get; set; }
+
+        public string Lastname { get; set; }
+
+        public string Issued { get; set; }
+        
+        public string DateOfIssued { get; set; }
+        
+        public string DepartmentCode { get; set; }
+        
+        public string SeriesAndNumber { get; set; }
+        
+        public string DateOfBirth { get; set; }
+        
+        public string PlaceOfBirth { get; set; }
+    }
+}
